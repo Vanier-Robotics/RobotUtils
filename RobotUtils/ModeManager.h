9e783ef2fdaab8874091daf17b521c8ce97aa849
Elisa-Moscato <114267307+Elisa-Moscato@users.noreply.github.com>
2023-01-05 12:15:46 -0500

    Setup modeManager class

--- RobotUtils/ModeManager.h
+++ RobotUtils/ModeManager.h
@@ -1,6 +1,6 @@
 /**
  * @file ModeManager.h
- * @author 
+ * @author Elisa Moscato
  * @brief ??
  * 
  * @copyright Copyright (c) 2023 Vanier Robotics (MIT License)
@@ -8,9 +8,44 @@
 
 #ifndef _INCLUDE_ROU_MODE_MANAGER_H_
 #define _INCLUDE_ROU_MODE_MANAGER_H_
+#include "Arduino.Extra.h"
+#include "CrcLib.h"
+#include "ArxSmartPtr.h"
 
 namespace rou
 {
+    class ModeManager
+    {
+        private:
+        Mode* m_nextMode = nullptr;
+        Mode* m_curentMode = nullptr;
+
+
+        public:
+        void update(float dt)
+        {
+            if(m_currentMode)
+            {
+                m_currentMode->update(dt);
+            }
+
+            if(m_nextMode)
+            {
+                if(m_curentMode)
+                m_currentMode-> unload();
+
+                m_currentMode = m_nextMode;
+                m_currentMode-> load();
+                m_nextMode = nullptr;
+            }
+        }
+        
+        void changeMode(Mode*nextMode)
+        {
+            m_nextMode = nextMode;
+        }
+
+    }
 
 } // rou
 
