1b6a9a1e5f1212a254b5bbc68f5d64b44fad842a
Jiucheng Zang <zangjiucheng@gmail.com>
2023-01-11 10:39:40 -0500

    Update(01/11/23)

---
+++ RobotUtils/Handle/Test_Drive/ArduinoExtra/ArduinoExtra.h
@@ -0,0 +1,16 @@
+/**
+ * @file ArduinoExtra.h
+ * @author Eliot Fondere
+ * @brief Main header file for Vanier Robotics' ArduinoExtra library
+ * 
+ * @copyright Copyright (c) 2022 Vanier Robotics (MIT License)
+ */
+
+#ifndef _INCLUDE_AEX_ARDUINO_EXTRA_H_
+#define _INCLUDE_AEX_ARDUINO_EXTRA_H_
+
+#include "ArduinoExtra/Functional.h"
+#include "ArduinoExtra/Array.h"
+#include "ArduinoExtra/Vector.h"
+
+#endif // _INCLUDE_AEX_ARDUINO_EXTRA_H_
