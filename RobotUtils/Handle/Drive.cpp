933dc295405a22f4c62dbd3cb313a1f42932072e
Jiucheng Zang <zangjiucheng@gmail.com>
2023-01-11 22:45:04 -0500

    Remove Drive.cpp (01/11/23)

--- RobotUtils/Handle/Drive.cpp
+++
@@ -1,9 +0,0 @@
-#include "Handle_Manager.cpp"
-#include <iostream>
-using namespace std;
-int main() {
-  Handle_Manager hm = Handle_Manager();
-  Handle handle = Handle();
-  hm.addHandle(handle);
-  hm.to_string();
-}
