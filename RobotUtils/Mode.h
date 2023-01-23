793c0b8a48e0a72f7701acdf7e3d475ce05c3f6d
efondere <ef@eliotfondere.com>
2023-01-23 16:19:33 -0500

    Format Mode and Mode Manager

--- RobotUtils/Mode.h
+++ RobotUtils/Mode.h
@@ -2,29 +2,30 @@
  * @file Mode.h
  * @author Elisa Moscato
  * @brief The base of modes
- * 
+ *
  * @copyright Copyright (c) 2023 Vanier Robotics (MIT License)
  */
 
 #ifndef _INCLUDE_ROU_MODE_H_
 #define _INCLUDE_ROU_MODE_H_
-#include "ArduinoExtra.h"
+
+#include <ArduinoExtra.h>
 
 namespace rou
 {
-    class Mode
-    {
-        protected:
-        aex::Vector <Module> m_modules;
-        Controller m_controller;
-        
-        public:
-        virtual void update(float dt) = 0;
-        virtual void setup (float dt) = 0;
 
-    }
+class Mode
+{
+public:
+	virtual void update(float dt)	= 0;
 
+	virtual void setup(float dt)	= 0;
 
-} // rou
+protected:
+	aex::Vector<Module>	m_modules;
+	Controller			m_controller;
+}
+
+} // namespace rou
 
 #endif // _INCLUDE_ROU_MODE_H_
--- RobotUtils/ModeManager.h
+++ RobotUtils/ModeManager.h
@@ -2,49 +2,51 @@
  * @file ModeManager.h
  * @author Elisa Moscato
  * @brief Allows to switch between different modes
- * 
+ *
  * @copyright Copyright (c) 2023 Vanier Robotics (MIT License)
  */
 
 #ifndef _INCLUDE_ROU_MODE_MANAGER_H_
 #define _INCLUDE_ROU_MODE_MANAGER_H_
+
 #include "Mode.h"
 
 namespace rou
 {
-    class ModeManager
-    {
-        private:
-        Mode* m_nextMode = nullptr;
-        Mode* m_curentMode = nullptr;
 
-        public:
-        void update(float dt)
-        {
-            if(m_currentMode)
-            {
-                m_currentMode->update(dt);
-            }
+class ModeManager
+{
+public:
+	void update(float dt)
+	{
+		if (m_currentMode)
+		{
+			m_currentMode->update(dt);
+		}
 
-            if(m_nextMode)
-            {
-                if(m_curentMode)
-                m_currentMode->unload();
+		if (m_nextMode)
+		{
+			if (m_currentMode)
+			{
+				m_currentMode->unload();
+			}
 
-                m_currentMode = m_nextMode;
-                m_currentMode->load();
-                m_nextMode = nullptr;
-            }
-        }
-        
-        void changeMode(Mode* nextMode)
-        {
-            m_nextMode = nextMode;
-        }
+			m_currentMode = m_nextMode;
+			m_currentMode->load();
+			m_nextMode = nullptr;
+		}
+	}
 
-    }
+	void changeMode(Mode* nextMode)
+	{
+		m_nextMode = nextMode;
+	}
 
+private:
+	Mode* m_nextMode	= nullptr;
+	Mode* m_currentMode	= nullptr;
+}
 
-} // rou
+} // namespace rou
 
 #endif // _INCLUDE_ROU_MODE_MANAGER_H_
