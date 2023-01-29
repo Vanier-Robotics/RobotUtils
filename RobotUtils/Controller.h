4bd102bc3320d9e4ba37e1459ffd353c5b04a702
efondere <ef@eliotfondere.com>
2023-01-29 17:08:33 -0500

    Prepare controller for merge

--- RobotUtils/Controller.h
+++ RobotUtils/Controller.h
@@ -16,19 +16,19 @@
 namespace rou
 {
 
-class Controller:
+class Controller
 {
 	// Structure that contains the variables and fuctions for the digital binding
 	struct DigitalBinding
 	{
-		BUTTON 						buttonID; //variable of type BUTTON (from CRC lib) that stores a specific button
+		Crc::BUTTON 				buttonID; //variable of type BUTTON (from CRC lib) that stores a specific button
 		aex::Function<void(bool)>	callback; //pointer function used to do some action with a digital button
 	};
 
 	//Structure that contains the variables and fuctions for the toggle binding
 	struct ToggleBinding
 	{
-		BUTTON						buttonID; //variable of type BUTTON (from CRC lib) that stores a specific button
+		Crc::BUTTON					buttonID; //variable of type BUTTON (from CRC lib) that stores a specific button
 		aex::Function<void(bool)>	callback; //pointer function used to do some action with a toggle action
 
 		bool isToggled	= false; //boolean variable that stores the state of the button (Toggled = true and not toggled = false)
@@ -40,7 +40,7 @@ class Controller:
 	//Structure that contains the variables and fuctions for the analog binding
 	struct AnalogBinding
 	{
-		ANALOG 						analogID; //variable of type ANALOG (from CRC lib) that stores a specific button
+		Crc::ANALOG 				analogID; //variable of type ANALOG (from CRC lib) that stores a specific button
 		aex::Function<void(int8_t)>	callback; //pointer function used to do some action with an analog button
 	};
 
@@ -61,33 +61,33 @@ class Controller:
 public:
 	//callback functions (pointer functions) of the different structures
 	//callback function for digital bindings
-	void digitalBind(BUTTON buttonID, aex::Function<void(bool)> callback)
+	void digitalBind(Crc::BUTTON buttonID, aex::Function<void(bool)> callback)
 	{
 		m_digitalBindings.pushBack({buttonID, callback});
 	}
 
 	//callback function for toggle bindings
-	void toggleBinding(BUTTON buttonID, aex::Function<void(bool)> callback)
+	void toggleBinding(Crc::BUTTON buttonID, aex::Function<void(bool)> callback)
 	{
-		m_analogBindings.pushBack({buttonID, callback});
+		m_toggleBindings.emplaceBack(buttonID, callback);
 	}
 
 	//callback function for analog bindings
-	void analogBinding(ANALOG analogID, aex::Function<void(int8_t)> callback)
+	void analogBinding(Crc::ANALOG analogID, aex::Function<void(int8_t)> callback)
 	{
-		m_toggleBindings.pushBack({analogID, callback});
+		m_analogBindings.pushBack({analogID, callback});
 	}
 
 	//callback function for digital sensor bindings
-	void digitalSensorBinding(unsigned int pin, aex::Function<void(bool)> callback)
+	void digitalSensorBinding(uint8_t pin, aex::Function<void(bool)> callback)
 	{
-		m_digitalSensorBindings.pushBack(pin, callback);
+		m_digitalSensorBindings.pushBack({pin, callback});
 	}
 
 	//callback function for analog sensor binding
-	void analogSensorBinding(unsigned int pin, aex::Function<void(unsigned int)> callback)
+	void analogSensorBinding(uint8_t pin, aex::Function<void(unsigned int)> callback)
 	{
-		m_analogSensorBindings.pushBack(pin, callback);
+		m_analogSensorBindings.pushBack({pin, callback});
 	}
 
 	void update()
@@ -96,59 +96,52 @@ public:
 		//Digital Bindings verification
 		for (int i = 0; i < m_digitalBindings.getSize(); i++)
 		{
-			if (CrcLib::ReadDigitalChannel(m_digitalBindings[i].buttonID))
-			{
-				bool result = CrcLib::ReadDigitalChannel(m_digitalBindings[i].buttonID);
-				m_digitalBindings[i].callback(result);
-			}
+			m_digitalBindings[i].callback(
+				Crc::CrcLib::ReadDigitalChannel(m_digitalBindings[i].buttonID));
 		}
 
 		//Analog Bindings verification
 		for (int i = 0; i < m_analogBindings.getSize(); i++)
 		{
-			int8_t result = CrcLib::ReadAnalogChannel(m_analogBindings[i].buttonID);
-			m_analogBindings[i].callback(result);
+			m_analogBindings[i].callback(
+				Crc::CrcLib::ReadAnalogChannel(m_analogBindings[i].analogID));
 		}
 
 		//Toggle Bindings verification
 		for (int i = 0; i < m_toggleBindings.getSize(); i++)
 		{
-			if(lastValue != CrcLib::ReadDigitalChannel(m_toggleBindings[i].buttonID)
-				&& CrcLib::ReadDigitalChannel(m_toggleBindings[i].buttonID))
+			bool status = Crc::CrcLib::ReadDigitalChannel(m_toggleBindings[i].buttonID);
+			if (m_toggleBindings[i].lastValue != status && status)
 			{
-				isToggle != isToggle;
-				m_toggleBindings[i].callback(isToggle);
+				m_toggleBindings[i].isToggled != m_toggleBindings[i].isToggled;
+				m_toggleBindings[i].callback(m_toggleBindings[i].isToggled);
 			}
 
-			lastValue = CrcLib::ReadDigitalChannel(m_toggleBindings[i].buttonID);
+			m_toggleBindings[i].lastValue = status;
 		}
 
 		//Digital Sensor Bindings verification
 		for (int i = 0; i < m_digitalSensorBindings.getSize(); i++)
 		{
-			if (CrcLib::GetDigitalInput(m_digitalSensorBindings[i].pin))
-			{
-				bool result =
-					(CrcLib::ReadDigitalChannel(m_digitalBindings[i].buttonID) == CrcLib::HIGH);
-				m_digitalBindings[i].callback(result);
-			}
+			m_digitalSensorBindings[i].callback(
+				(Crc::CrcLib::GetDigitalInput(m_digitalSensorBindings[i].pin) == HIGH));
 		}
 
 		//Analog Sensor Bindings verification
 		for (int i = 0; i < m_analogSensorBindings.getSize(); i++)
 		{
-			unsigned int pin = CrcLib::ReadAnalogChannel(m_digitalBindings[i].buttonID);
-			m_digitalBindings[i].callback(pin);
+			m_analogSensorBindings[i].callback(
+				Crc::CrcLib::GetAnalogInput(m_analogSensorBindings[i].pin));
 		}
 	}
 
 private:
 	//Vector arrays
-	aex::Vector<DigitalBinding>			m_digitalBindings(); //Create a vector array of type DigitalBinding
-	aex::Vector<ToggleBinding>			m_toggleBindings(); //Create a vector array of type ToggleBinding
-	aex::Vector<AnalogBinding>			m_analogBindings(); //Create a vector array of type AnalogBinding
-	aex::Vector<DigitalSensorBinding>	m_digitalSensorBindings(); //Create a vector array of type DigitalSensorBinding
-	aex::Vector<AnalogSensorBinding>	m_analogSensorBindings(); //Create a vector array of type AnalogBinding
+	aex::Vector<DigitalBinding>			m_digitalBindings; //Create a vector array of type DigitalBinding
+	aex::Vector<ToggleBinding>			m_toggleBindings; //Create a vector array of type ToggleBinding
+	aex::Vector<AnalogBinding>			m_analogBindings; //Create a vector array of type AnalogBinding
+	aex::Vector<DigitalSensorBinding>	m_digitalSensorBindings; //Create a vector array of type DigitalSensorBinding
+	aex::Vector<AnalogSensorBinding>	m_analogSensorBindings; //Create a vector array of type AnalogBinding
 };
 
 } // rou
