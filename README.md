b777f8c4bdcd50a60df123899697f39b3b9af27c
efondere <ef@eliotfondere.com>
2023-02-06 12:00:43 -0500

    Use an Encoder object in EncoderHandle

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Setup
 
 1. Install the [Arduino IDE](https://www.arduino.cc/en/software). Use the new 2.0.0 version if possible as it is easier to work with.
-2. Install CrcLib and ArduinoExtra (see programming resources document)
+2. Install CrcLib, ArduinoExtra (see programming resources document) and [Paul Stoffergen's Encoder library](https://www.arduino.cc/reference/en/libraries/encoder/)
 3. Clone this repository in the User/Documents/Arduino/libraries/ folder (or equivalent on Mac).
 4. Open the Arduino IDE and create a new project. Select the `Arduino Mega or Mega 2560` board. Write `#include <RobotUtils.h>` at the top of the file and press verify to make sure everything is installed properly.
 5. Open the cloned repository in VSCode or another text editor/IDE (make sure you don't commit your project files on accident). You can install an editorconfig extension for automatic file formatting ([VSCode Example](https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig)).
--- RobotUtils/Handles/EncoderHandle.h
+++ RobotUtils/Handles/EncoderHandle.h
@@ -2,11 +2,9 @@
  * @file EncodereHandle.h
  * @author Jiucheng Zang
  * @brief Encoder Handle
- * @version 0.1
- * @date 2023-01-30
- * 
+ *
  * @copyright Copyright (c) 2023 Vanier Robotics (MIT License)
- * 
+ *
  */
 
 #ifndef _INCLUDE_ROU_ENCODER_HANDLE_H_
@@ -21,7 +19,7 @@ namespace rou
 
 /**
  * @brief Encoder Handle class
- * 
+ *
  * @extends Handle class
  */
 class EncoderHandle : public Handle
@@ -29,87 +27,46 @@ class EncoderHandle : public Handle
 public:
     /**
      * @brief Construct a new Encoder Handle object with one interrupt
-     * 
+     *
      * @param PinA Singal A
      * @param PinB SIngle B
      */
     EncoderHandle(int8_t pinA, int8_t pinB)
+	: m_encoder(pinA, pinB)
     {
-        m_pin_a = pinA;
-        m_pin_b = pinB;
-        position = 0;
+        m_pinA = pinA;
+        m_pinB = pinB;
     }
 
-    /**
-     * @brief Construct a new Encoder Handle object with one interrupt (Given initial position)
-     * 
-     * @param pinA 
-     * @param pinB 
-     * @param initialPositon 
-     */
-    EncoderHandle(int8_t pinA, int8_t pinB, long int initialPositon) 
-    {
-        m_pin_a = pinA;
-        m_pin_b = pinB;
-        position = initialPositon;
-    }
-
-    /**
-     * @brief Update Status of Encoder
-     * 
-     */
-    void UpdateEncoder()
-    {
-        if (digitalRead(m_pin_a)) digitalRead(m_pin_b) ? position++ : position--;
-        else digitalRead(m_pin_b) ? position-- : position++;
-    }
-    
     /**
      * @brief Get the Position object
-     * 
-     * @return long int 
+     *
+     * @return long int
      */
-    long int getPosition () {
-      return position;
-    };
-
-    /**
-     * @brief Set the Position object
-     * 
-     * @param p 
-     */
-    void setPosition (const long int p) {
-      position = p;
-    };
-
-    /**
-     * @brief Get the Pin object
-     * 
-     * @return uint8_t* 
-     */
-    uint8_t * getPin()
+    int32_t getPosition()
 	{
-        int8_t Pin = {m_pin_a,m_pin_b};
-		return Pin;
+		m_encoder.read();
+    }
+
+    /**
+     * @brief Get the A pin
+     *
+     * @return uint8_t
+     */
+    uint8_t getPinA()
+	{
+        return m_pinA;
 	}
 
-protected:
-    /**
-     * @brief Setup For Encoder
-     * 
-     */
-	void setup() override
+	uint8_t getPinB()
 	{
-        pinMode(m_pin_a, INPUT);
-        pinMode(m_pin_b, INPUT);
-        digitalWrite(m_pin_a, HIGH); // and turn on pull-up resistors
-        digitalWrite(m_pin_b, HIGH);
+		return m_pinB;
 	}
 
 private:
-    long int position;
-    int8_t m_pin_a;
-    int8_t m_pin_b;
+    Encoder m_encoder;
+    int8_t m_pinA;
+    int8_t m_pinB;
 };
 
 } // namespace rou
